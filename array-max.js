369ee4fffcfd2bdf3bc6373201e21983e118865a
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-02-23 16:19:30 +0600

    array-max added

---
+++ array-max.js
@@ -0,0 +1,12 @@
+function arrayMax(numbers) {
+  let max = 0;
+  for (const number of numbers) {
+    if (number > max) {
+      max = number;
+    }
+  }
+  return max;
+}
+
+const numberList = [52, 25, 3, 55, 4, 54, 45, 69, 89, 100, 58, 101];
+console.log(arrayMax(numberList));
